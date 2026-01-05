1d2bfe5b4bc27afae21f5c3b8642ef357c429545
Siwakon <tiger.prasang@gmail.com>
2026-01-05 17:45:49 +0700

    Update

--- src/yahboom_bringup/params/car_2d.lua
+++ src/yahboom_bringup/params/car_2d.lua
@@ -8,19 +8,19 @@ options = {
   tracking_frame = "base_footprint_sync",
   published_frame = "base_footprint_sync",
   odom_frame = "odom",
-  -- true改为false，不用提供里程计数据
+  -- Change true to false if you don't want to provide odometry data
   provide_odom_frame = true,
-  -- false改为true，仅发布2D位资
+  -- Change false to true to publish only 2D pose
   publish_frame_projected_to_2d = true,
-  -- false改为true，使用里程计数据
+  -- Change false to true to use odometry data
   use_odometry = false,
   use_nav_sat = false,
   use_landmarks = false,
-  -- 0改为1,使用一个雷达
+  -- Change 0 to 1 to use one laser scanner
   num_laser_scans = 1,
-  -- 1改为0，不使用多波雷达
+  -- Change 1 to 0 to not use multi-echo laser scans
   num_multi_echo_laser_scans = 0,
-  -- 10改为1，1/1=1等于不分割
+  -- Change 10 to 1, 1/1=1 means no subdivision
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
@@ -35,28 +35,28 @@ options = {
 }
 
 
--- false改为true，启动2D SLAM
+-- Change false to true to enable 2D SLAM
 MAP_BUILDER.use_trajectory_builder_2d = true
 
--- 0改成0.10,比机器人半径小的都忽略
+-- Change 0 to 0.10, ignore all measurements smaller than robot radius
 TRAJECTORY_BUILDER_2D.min_range = 0.10
--- 30改成3.5,限制在雷达最大扫描范围内，越小一般越精确些
+-- Change 30 to 3.5, limit to maximum laser scan range, smaller values generally more accurate
 TRAJECTORY_BUILDER_2D.max_range = 3.5
--- 5改成3,传感器数据超出有效范围最大值
+-- Change 5 to 3, maximum value for sensor data exceeding valid range
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
--- true改成false,不使用IMU数据，大家可以开启，然后对比下效果
+-- Change true to false to not use IMU data, you can enable it and compare the results
 TRAJECTORY_BUILDER_2D.use_imu_data = false
--- false改成true,使用实时回环检测来进行前端的扫描匹配
+-- Change false to true to use real-time loop closure detection for front-end scan matching
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true 
--- 1.0改成0.1,提高对运动的敏感度
+-- Change 1.0 to 0.1 to increase sensitivity to motion
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
 
--- 0.55改成0.65,Fast csm的最低分数，高于此分数才进行优化。
+-- Change 0.55 to 0.65, minimum score for Fast CSM, optimization only occurs above this score
 POSE_GRAPH.constraint_builder.min_score = 0.65
---0.6改成0.7,全局定位最小分数，低于此分数则认为目前全局定位不准确
+-- Change 0.6 to 0.7, minimum score for global localization, below this score indicates inaccurate global localization
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
 
--- 设置0可关闭全局SLAM
+-- Set to 0 to disable global SLAM
 POSE_GRAPH.optimize_every_n_nodes = 0
 
 return options
